a7c2e28f2ba86588367bb577298840ca56aa62f7
Kitiphop Sophakhayang <Kitiphop.sophakhayang@gmail.com>
2022-10-06 20:20:59 +0700

    first commit

---
+++ test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<form action="<?=$_SERVER['PHP_SELF'];?>" method="POST">
+กรุณาใส่รัศมีของวงกลม <input type="text" name="v"> 
+<input type="submit" value="คำนวณ">
+
+<?php
+    isset( $_POST['v'] ) ? $v = $_POST['v'] : $v = "";
+    if( !empty( $v ) ) {
+        echo "<hr/>";
+        $pi = 3.14;
+        $total = $pi * ( $v * $v );
+        echo "พื้นที่วงกลม = ".number_format( $total, 2 );
+    }
+?>
+
+</form>
+</body>
+</html>
